4e95bb8eb659db65d4b956ad4bdf83e1526634b8
EarthlyEric6 <earthlyeric6@gmail.com>
2024-11-11 10:41:14 +0800

    edit method to get biome id

--- src/main/java/kaptainwutax/seedcrackerX/util/BiomeFixer.java
+++ src/main/java/kaptainwutax/seedcrackerX/util/BiomeFixer.java
@@ -5,6 +5,7 @@ import com.seedfinding.mcbiome.biome.Biomes;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.registry.BuiltinRegistries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -21,7 +22,6 @@ public class BiomeFixer {
         for (Biome biome : Biomes.REGISTRY.values()) {
             COMPATREGISTRY.put(biome.getName(), biome);
         }
-        //renamed
         COMPATREGISTRY.put("snowy_plains", Biomes.SNOWY_TUNDRA);
         COMPATREGISTRY.put("old_growth_birch_forest", Biomes.TALL_BIRCH_FOREST);
         COMPATREGISTRY.put("old_growth_pine_taiga", Biomes.GIANT_TREE_TAIGA);
@@ -32,7 +32,7 @@ public class BiomeFixer {
         COMPATREGISTRY.put("windswept_savanna", Biomes.SHATTERED_SAVANNA);
         COMPATREGISTRY.put("sparse_jungle", Biomes.JUNGLE_EDGE);
         COMPATREGISTRY.put("stony_shore", Biomes.STONE_SHORE);
-        //new
+        // 新增的生態群系對應
         COMPATREGISTRY.put("meadow", Biomes.PLAINS);
         COMPATREGISTRY.put("grove", Biomes.TAIGA);
         COMPATREGISTRY.put("snowy_slopes", Biomes.SNOWY_TUNDRA);
@@ -40,18 +40,14 @@ public class BiomeFixer {
         COMPATREGISTRY.put("jagged_peaks", Biomes.TAIGA);
         COMPATREGISTRY.put("stony_peaks", Biomes.TAIGA);
         COMPATREGISTRY.put("mangrove_swamp", Biomes.SWAMP);
-
-        //unsure what to do with those, they'll return THE_VOID for now
-        //dripstone_caves
-        //lush_caves
-        //deep_dark
     }
 
     public static Biome swap(net.minecraft.world.biome.Biome biome) {
         ClientPlayNetworkHandler networkHandler = MinecraftClient.getInstance().getNetworkHandler();
         if (networkHandler == null) return Biomes.VOID;
 
-        Identifier biomeID = networkHandler.getRegistryManager().get(RegistryKeys.BIOME).getId(biome);
+        var biomeRegistry = networkHandler.getRegistryManager().get(RegistryKeys.BIOME);
+        Identifier biomeID = biomeRegistry.getId(biome);
 
         if (biomeID == null) return Biomes.THE_VOID;
 
@@ -59,8 +55,7 @@ public class BiomeFixer {
     }
 
     public static net.minecraft.world.biome.Biome swap(Biome biome) {
-        // internal, meh
-        var biomeRegistries = BuiltinRegistries.createWrapperLookup().getWrapperOrThrow(RegistryKeys.BIOME);
+        var biomeRegistries = BuiltinRegistries.REGISTRY.get(RegistryKeys.BIOME);
 
         return biomeRegistries.getOptional(RegistryKey.of(RegistryKeys.BIOME, Identifier.ofVanilla(biome.getName()))).orElse(
                 biomeRegistries.getOrThrow(BiomeKeys.THE_VOID)
